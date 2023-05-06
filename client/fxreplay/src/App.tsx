6b2f4a20062ca9ca1070fff1a71a021942f83385
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-05-07 00:47:26 +0200

    [init]: completed backend project initialisation

--- client/fxreplay/src/App.tsx
+++ client/fxreplay/src/App.tsx
@@ -3,18 +3,14 @@ import AppWrapper from "./components/global/AppWrapper";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import TestWidget from "./pages/Test";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 
 function App() {
   return (
-    <Provider store={store}>
       <AppWrapper>
         <Route path="/" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/test-trade" element={<TestWidget />} />
       </AppWrapper>
-    </Provider>
   );
 }
 
